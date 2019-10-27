14eeee5004777ca5821efeed129e198b8e6bc56e
Jacttis <48808328+Jacttis@users.noreply.github.com>
2019-10-27 01:44:33 -0300

    Metodos realizados Partida

--- Partida/partida.c
+++ Partida/partida.c
@@ -1,7 +1,12 @@
 //
-// Created by julia on 10/26/2019.
+// Created by julian on 10/26/2019.
 //
 #include "partida.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+void printTablero(int grilla[][3],int filas);
 /**
 Inicializa una nueva partida, indicando:
  - Modo de partida (Usuario vs. Usuario o Usuario vs. Agente IA).
@@ -9,16 +14,75 @@ Inicializa una nueva partida, indicando:
  - Nombre que representa al Jugador 1.
  - Nombre que representa al Jugador 2.
 **/
-extern void nueva_partida(tPartida * p, int modo_partida, int comienza, char * j1_nombre, char * j2_nombre);
+extern void nueva_partida(tPartida * p, int modo_partida, int comienza, char * j1_nombre, char * j2_nombre){
+    (*p)=(tPartida)malloc(sizeof(struct partida));
+    (*p)->tablero=(tTablero)malloc(sizeof(struct tablero));
+    for(int i=0;i<3;i++){
+        for(int j=0;i<3;i++){
+            (*p)->tablero->grilla[i][j]=PART_SIN_MOVIMIENTO;
+        }
+    }
+    (*p)->modo_partida=modo_partida;
+    if(comienza==PART_JUGADOR_RANDOM){
+        int random=rand()%2;
+        if(random==0){
+            (*p)->turno_de=PART_JUGADOR_1;
+        }
+        else{
+            (*p)->turno_de=PART_JUGADOR_2;
+        }
+
+    }
+    else{
+        (*p)->turno_de=comienza;
+    }
+    strcpy((*p)->nombre_jugador_1,j1_nombre);
+    strcpy((*p)->nombre_jugador_2,j2_nombre);
+}
 
 /**
 Actualiza, si corresponde, el estado de la partida considerando que el jugador al que le corresponde jugar, decide hacerlo en la posici�n indicada (X,Y).
 En caso de que el movimiento a dicha posici�n sea posible, retorna PART_MOVIMIENTO_OK; en caso contrario, retorna PART_MOVIMIENTO_ERROR.
 Las posiciones (X,Y) deben corresponderse al rango [0-2]; X representa el n�mero de fila, mientras Y el n�mero de columna.
 **/
-extern int nuevo_movimiento(tPartida p, int mov_x, int mov_y);
+extern int nuevo_movimiento(tPartida p, int mov_x, int mov_y){
+    if(mov_x>2 || mov_x<0 || mov_y>2 || mov_y<0){
+        return PART_MOVIMIENTO_ERROR;
+    }
+    if (p->tablero->grilla[mov_x][mov_y] == PART_SIN_MOVIMIENTO) {
+        p->tablero->grilla[mov_x][mov_y] = p->turno_de;
+        printTablero(p->tablero->grilla[3][3],3);
+        p->turno_de = p->turno_de == PART_JUGADOR_1? PART_JUGADOR_2: PART_JUGADOR_1;
+
+    }
+    else{
+        return PART_MOVIMIENTO_ERROR;
+    }
+    return PART_MOVIMIENTO_OK;
+}
 
 /**
 Finaliza la partida referenciada por P, liberando toda la memoria utilizada.
 **/
-extern void finalizar_partida(tPartida * p);
+extern void finalizar_partida(tPartida * p){
+    free((*p)->tablero);
+    free(*p);
+}
+
+void printTablero(int grilla[][3],int filas) {
+    printf("\n");   //MEJORAARRrrrr???
+    for (int  i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (grilla[i][j] == PART_SIN_MOVIMIENTO)
+                printf("  -");
+            else if (grilla[i][j] == PART_JUGADOR_1)
+                printf(" X-");
+            else
+                printf(" O-");
+        }
+        if (i != 2)
+            printf("\n");
+        else
+            printf("\n\n");
+    }
+}
